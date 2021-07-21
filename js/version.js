9c01d6ec5c17b13fca0c21389c27cfc43a6f43bc
Ramón Taboada <rntaboada@yahoo.com.ar>
2021-07-21 08:22:35 -0300

    corrección de la versión en version.js

--- js/version.js
+++ js/version.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var version = "RT v2.1.1"
+    var version = "RT v2.1.0"
     var fecha = "2021-05-03"
     $("label.version").text(version + " / " + fecha);
 });
