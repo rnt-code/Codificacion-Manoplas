9a1dd1f207aa597df18dbd37c8fb514c17e9064d
Ramón Taboada <rntaboada@yahoo.com.ar>
2021-07-30 12:52:35 -0300

    version nueva 2.4.0

--- js/version.js
+++ js/version.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var version = "RT v2.2.0"
-    var fecha = "2021-07-21"
+    var version = "RT v2.4.0"
+    var fecha = "2021-07-30"
     $("label.version").text(version + " / " + fecha);
 });
